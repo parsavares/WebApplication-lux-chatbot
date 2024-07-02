082eaa91d4f1a9a624f0365ce2888bde1bb8be41
parsavares <parsavares@yahoo.com>
2024-07-02 18:01:06 +0200

    Home

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/uni.png'; // Ensure the logo is placed in the src/assets directory
+import logo from '../assets/uni.svg'; // Ensure the logo is placed in the src/assets directory
 
 const Footer: React.FC = () => {
     return (
@@ -8,9 +8,9 @@ const Footer: React.FC = () => {
                 <img src={logo} alt="University Logo" className="h-8 w-8" />
                 <span className="text-lg">Universite de Luxembourg</span>
             </div>
-            <div className="flex space-x-4">
-                <a href="#" className="hover:underline">HELP</a>
-                <a href="#" className="hover:underline">Contact</a>
+            <div className="flex space-x-4"> 
+                <a href="https://github.com/othmane-mahfoud/luxtutor-front" target="_blank" rel="noopener noreferrer" className="hover:underline">HELP</a>
+                <a href="https://github.com/othmane-mahfoud/luxtutor-front?tab=readme-ov-file#contact" target="_blank" rel="noopener noreferrer" className="hover:underline">Contact</a>
             </div>
         </footer>
     );
--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/uni.png'; // Ensure the logo is placed in the src/assets directory
+import logo from '../assets/uni.svg'; // Ensure the logo is placed in the src/assets directory
 
 const Header: React.FC = () => {
     return (
@@ -9,9 +9,9 @@ const Header: React.FC = () => {
                 <span className="text-2xl font-bold">Home</span>
             </div>
             <nav className="flex space-x-6">
-                <a href="#" className="hover:underline">Project</a>
-                <a href="#" className="hover:underline">Features</a>
-                <a href="#" className="hover:underline">Contact</a>
+                <a href="https://github.com/othmane-mahfoud/luxtutor-front" target="_blank" rel="noopener noreferrer" className="hover:underline">Project</a>
+                <a href="https://github.com/othmane-mahfoud/luxtutor-front?tab=readme-ov-file#contact" target="_blank" rel="noopener noreferrer" className="hover:underline">Contact</a>
+
             </nav>
             <div>
                 <a href="#" className="hover:underline">Login In</a> |
