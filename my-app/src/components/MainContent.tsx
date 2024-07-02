1ea10ba5801e8cd0913f15bd28e98163fd08a2d1
parsavares <parsavares@yahoo.com>
2024-07-02 16:28:21 +0200

    Home

--- my-app/src/components/MainContent.tsx
+++ my-app/src/components/MainContent.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import luxembourgFlag from '../assets/luxembourgFlag.jpg'; // Ensure the flag image is placed in the src/assets directory
+import luxembourgFlag from '../assets/luxembourgFlag.gif'; // Ensure the GIF file is placed in the src/assets directory
 
 const MainContent: React.FC = () => {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100">
-            <img src={luxembourgFlag} alt="Luxembourg Flag" className="mb-4 w-1/3 h-auto rounded-lg shadow-lg" />
+            <img src={luxembourgFlag} alt="Luxembourg Flag"/>
             <h1 className="text-4xl font-bold my-4 text-gray-800">Welcome!</h1>
             <h2 className="text-2xl font-bold my-4 text-gray-800">Luxembourgish Language Learning</h2>
-            <button className="bg-blue-500 text-white py-3 px-6 rounded-full shadow-md hover:bg-blue-600 transition transform hover:scale-105 ">
-                    Introduction to the Project
-                </button>
-
+            <button className="bg-blue-500 text-white py-3 px-6 rounded-full shadow-md hover:bg-blue-600 transition transform hover:scale-105">
+                Introduction to the Project
+            </button>
             <div className="flex justify-center space-x-8 my-8">
                 <button className="bg-blue-500 text-white py-3 px-6 rounded-full shadow-md hover:bg-blue-600 transition transform hover:scale-105">Feature 1</button>
                 <button className="bg-blue-500 text-white py-3 px-6 rounded-full shadow-md hover:bg-blue-600 transition transform hover:scale-105">Feature 2</button>
