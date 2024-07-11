5ef1ff0da5fbcad4374105e9f9555d1d20352b2c
parsavares <parsavares@yahoo.com>
2024-07-11 13:21:48 +0200

    Add additional fields to SignUp and Profile components

--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,40 @@
 import React, { useContext, useState } from 'react';
 import { AuthContext } from './context/AuthContext';
-import { Container, CssBaseline, Box, Avatar, Typography, Grid, TextField, Button } from '@mui/material';
+import { Container, CssBaseline, Box, Avatar, Typography, Grid, TextField, Button, FormControl, InputLabel, Select, MenuItem, FormControlLabel, Checkbox } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const Profile = () => {
     const { currentUser, updateProfile } = useContext(AuthContext);
+
     const [firstName, setFirstName] = useState(currentUser?.firstName || '');
     const [lastName, setLastName] = useState(currentUser?.lastName || '');
+    const [nationality, setNationality] = useState(currentUser?.nationality || '');
+    const [nativeLanguage, setNativeLanguage] = useState(currentUser?.nativeLanguage || '');
+    const [purpose, setPurpose] = useState(currentUser?.purpose || '');
+    const [level, setLevel] = useState(currentUser?.level || '');
+    const [preferences, setPreferences] = useState(currentUser?.preferences || []);
+
+    const handlePreferencesChange = (event) => {
+        const { value } = event.target;
+        setPreferences((prev) =>
+            prev.includes(value)
+                ? prev.filter((preference) => preference !== value)
+                : [...prev, value]
+        );
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        updateProfile(firstName, lastName)
+        const updatedUserData = {
+            firstName,
+            lastName,
+            nationality,
+            nativeLanguage,
+            purpose,
+            level,
+            preferences,
+        };
+        updateProfile(updatedUserData)
             .then(() => {
                 alert('Profile updated successfully');
             })
@@ -62,6 +86,90 @@ const Profile = () => {
                                 value={lastName}
                                 onChange={(e) => setLastName(e.target.value)}
                             />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="nationality"
+                                label="Nationality"
+                                name="nationality"
+                                value={nationality}
+                                onChange={(e) => setNationality(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="nativeLanguage"
+                                label="Native Language"
+                                name="nativeLanguage"
+                                value={nativeLanguage}
+                                onChange={(e) => setNativeLanguage(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel id="purpose-label">Purpose of Learning</InputLabel>
+                                <Select
+                                    labelId="purpose-label"
+                                    id="purpose"
+                                    name="purpose"
+                                    value={purpose}
+                                    onChange={(e) => setPurpose(e.target.value)}
+                                    label="Purpose of Learning"
+                                >
+                                    <MenuItem value="work">Work</MenuItem>
+                                    <MenuItem value="vacation">Vacation</MenuItem>
+                                    <MenuItem value="school">School</MenuItem>
+                                    <MenuItem value="business">Business</MenuItem>
+                                    <MenuItem value="traveling">Traveling</MenuItem>
+                                    <MenuItem value="all above">All Above</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel id="level-label">Level of Luxembourgish</InputLabel>
+                                <Select
+                                    labelId="level-label"
+                                    id="level"
+                                    name="level"
+                                    value={level}
+                                    onChange={(e) => setLevel(e.target.value)}
+                                    label="Level of Luxembourgish"
+                                >
+                                    <MenuItem value="beginner">Beginner</MenuItem>
+                                    <MenuItem value="intermediate">Intermediate</MenuItem>
+                                    <MenuItem value="advanced">Advanced</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl component="fieldset">
+                                <Typography component="legend">Preferences of Learning</Typography>
+                                <FormControlLabel
+                                    control={<Checkbox name="preferences" value="conversation" checked={preferences.includes('conversation')} onChange={handlePreferencesChange} />}
+                                    label="Conversation"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox name="preferences" value="listening" checked={preferences.includes('listening')} onChange={handlePreferencesChange} />}
+                                    label="Listening"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox name="preferences" value="reading" checked={preferences.includes('reading')} onChange={handlePreferencesChange} />}
+                                    label="Reading"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox name="preferences" value="questionAndAnswering" checked={preferences.includes('questionAndAnswering')} onChange={handlePreferencesChange} />}
+                                    label="Question and Answering"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox name="preferences" value="grammar" checked={preferences.includes('grammar')} onChange={handlePreferencesChange} />}
+                                    label="Grammar"
+                                />
+                            </FormControl>
                         </Grid>
                     </Grid>
                     <Button
